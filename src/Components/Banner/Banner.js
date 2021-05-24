b77ffba936bb1e7c4871e124dc9d6b945bc9e435
Nidhin P P <nedhinpp@gmail.com>
2021-05-24 22:37:28 +0530

    Initial Design

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Banner.css';
+
+function Banner() {
+    return (
+        <div className="banner">
+            <div className="content">
+               <h1 className="title">Movie Name</h1>
+                <div className="banner_buttons">
+                    <button className="button">Play</button>
+                    <button className="button">My List</button>   
+                </div> 
+                <h1 className="description">This is a clone of netflix as a part of react js tutorials</h1>
+            </div>
+        <div className="fade_bottom"></div>    
+        </div>
+    )
+}
+
+export default Banner
